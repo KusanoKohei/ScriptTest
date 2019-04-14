a82dd69ce8954de5ac0d0c6e68e2e1460b2a99a7
kusanokohei <kusanokohei@yahoo.co.jp>
2019-04-14 22:18:26 +0900

    Lesson4の課題を再提出します。
    配列の前にMagic関数を呼べば上手く表示できるのですが、配列の後にMagicを呼ぶとコンソールで上手く魔法の使用を表示してくれません。原因を調べていただけませんか。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -10,18 +10,20 @@ public class Boss
     //魔法攻撃の関数
     public void Magic(int cost)
     {
-        //残りmpを減らす
-        this.mp -= cost;
 
-        if (this.mp > 0)
+        if (this.mp > cost)
         {
-            //残りのhpを表示
+            //残りmpを減らす
+            this.mp -= cost;
+
+            //残りのmpを表示
             Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
         }
         else
         {
             Debug.Log("MPが足りないため魔法が使えない。");
         }
+        
     }
 }
 
@@ -32,21 +34,6 @@ public class Test : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
-        //要素数５の配列を初期化、値を代入する
-        int[] array = { 1, 2, 3, 4, 5 };
-
-        //配列の要素数の分だけ表示を繰り返す（昇順）
-        for (int i = 0; i < array.Length; i++)
-        {
-            Debug.Log(array[i]);
-        }
-
-        //配列の要素数の分だけ表示を繰り返す（降順）
-        for (int i = 4; i >= 0; i--)
-        {
-            Debug.Log(array[i]);
-        }
 
 
         //Bossクラスの変数を宣言してインスタンスを代入
@@ -60,6 +47,20 @@ public class Test : MonoBehaviour
         }
 
 
+        //要素数５の配列を初期化、値を代入する
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        //配列の要素数の分だけ表示を繰り返す（昇順）
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //配列の要素数の分だけ表示を繰り返す（降順）
+        for (int i = 4; i < array.Length; i--)
+        {
+            Debug.Log(array[i]);
+        }
     }
 
     // Update is called once per frame
